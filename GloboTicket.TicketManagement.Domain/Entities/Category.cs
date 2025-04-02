c70e66d872308d3da3f167e2e8c973692392a6e1
Ryan <ryanhorrmann123@gmail.com>
2025-04-02 09:47:02 +0200

    Add project files.

---
+++ GloboTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Domain.Common;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public ICollection<Event>? Events { get; set; }
+    }
+}
